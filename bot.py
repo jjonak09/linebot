04da43005dc337bcab74890ccb8dfced82bdd6eb
Jun Saitou <saitouatusi9415@gmail.com>
2019-01-09 16:00:19 +0900

    improve

--- bot.py
+++ bot.py
@@ -43,6 +43,11 @@ line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
