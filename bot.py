85407a329ea691a1fabf62153eddb55b496c15a7
Jun Saitou <saitouatusi9415@gmail.com>
2019-01-23 00:07:43 +0900

    improve

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_answers = get_answer()
-    TEXT = "いいぞ! {}" .format(line_answers["url"])
+    TEXT = "{}はいいぞ!".format(line_answers["name"])
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=TEXT)
