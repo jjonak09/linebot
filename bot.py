e0f6c5e9c7864258c3101c4345063915f8030eaf
Jun Saitou <saitouatusi9415@gmail.com>
2019-01-23 00:27:04 +0900

    improve

--- bot.py
+++ bot.py
@@ -62,7 +62,11 @@ def handle_message(event):
     # TEXT = []
     # TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
     # TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
-    TEXT = "{}はいいぞ! {}".format(line_answers["name"], line_answers["url"])
+    # TEXT = "{}はいいぞ! {}".format(line_answers["name"], line_answers["url"])
+    TEXT = [
+        ["{}はいいぞ!{}".format(data["name"], data["url"])],
+        ["{}はいいぞ!{}".format(data["name"], data["url"])]
+    ]
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=TEXT)
--- test.py
+++ test.py
@@ -7,9 +7,12 @@ URL = 'https://api.myjson.com/bins/dzzsg'
 # TEXT = []
 read = requests.get(URL)
 data = json.loads(read.text)
-TEXT = []
-TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
-TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
+TEXT = [
+    ["{}はいいぞ!{}".format(data["name"], data["url"])],
+    ["{}はいいぞ!{}".format(data["name"], data["url"])]
+]
+# TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
+# TEXT.append("{}はいいぞ!{}".format(data["name"], data["url"]))
 # TEXT.append(data["name"] + "はいいぞ")
 # TEXT.append(data["url"])
 print(TEXT)
