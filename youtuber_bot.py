069e391852a39e91fded9a213fabc20b82917658
Jun Saitou <saitouatusi9415@gmail.com>
2019-02-08 16:14:48 +0900

    improve

--- youtuber_bot.py
+++ youtuber_bot.py
@@ -8,16 +8,30 @@ import re
 
 app = Flask(__name__)
 
-URL = 'https://youtuberapi.herokuapp.com/api/vtuber/'
-JSON = 'format=json'
+URL = 'https://youtuberapi.herokuapp.com/api/'
+JSON_TYPE = '?format=json'
 Youtube_URL = 'https://www.youtube.com/channel/'
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    # res = make_message(req)
-    return make_response(jsonify({'fulfillmentText': 'hello'}))
+    res = make_message(req)
+    return make_response(jsonify({'fulfillmentText': res}))
+
+
+def make_message(req):
+    params = req['queryResult']['parameters']
+
+    if params['youtuber_vtuber'] == 'Vtuber':
+        url = URL + 'vtuber/' + JSON_TYPE
+    else if params['youtuber_vtuber'] == 'Youtuber':
+        url = URL + 'youtuber/' + JSON_TYPE
+
+    if params['youtuber_tag'] != "":
+        url = url + '&tag=' + params['youtuber_tag']
+
+    return url
 
 
 if __name__ == "__main__":
